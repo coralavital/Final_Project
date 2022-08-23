accc7ef267480ff45c9866b0897cb4d778169167
coralavital <coralavital24@gmail.com>
2022-08-23 19:49:05 +0300

    first commit

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,9 +4,9 @@ const String ipsumText =
 class AppConstants {
   static const String appName = 'DBFood';
   static const int appVersion = 1;
-  static const String baseUrl = 'http://mvs.bslmeiyu.com';
-  static const String popularProductUri = '/api/v1/products/popular';
-  static const String recommendedProductUri = '/api/v1/products/recommended';
+  static const String baseUrl = 'https://jsonplaceholder.typicode.com';
+  static const String popularProductUri = '/users';
+  static const String recommendedProductUri = '/users';
   static const String token = 'DBtoken';
   static const String uploadUrl = '/uploads/';
   static const String cartList = 'cart-list';
